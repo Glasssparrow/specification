c471a3348def3d2e30d1285d711354db1fa109a8
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-07-08 21:26:36 +0300

    add_local_library

--- read_library/local_library.py
+++ read_library/local_library.py
@@ -1,3 +1,5 @@
+from common.raw_data import get_data_with_first_column_as_index
+from pandas import isna
 
 
 def add_local_library(
@@ -11,8 +13,52 @@ def add_local_library(
 ):
     sheet_list = []
     for index in nodes_metadata.index:
-        if nodes_metadata.loc[index, "type"] == "specification":
+        if nodes_metadata.loc[index, "type"] == "library":
             sheet_list.append(index)
 
     for sheet_name in sheet_list:
-        pass
+        sheet = get_data_with_first_column_as_index(path, sheet_name)
+        for index in sheet.index:
+            if (sheet.loc[index, subcategory_column] not in
+                    library_metadata.data.index):
+                raise Exception(
+                    f"Не найдена категория "
+                    f"{sheet.loc[index, subcategory_column]}"
+                )
+            # Записываем подкатегорию.
+            library.loc[index, "subcategory"] = (
+                sheet.loc[index, subcategory_column])
+            # Вытаскиваем данные о подкатегории из метаданных.
+            for column in [
+                "category", "category_sort_priority",
+                "subcategory_sort_priority"
+            ]:
+                library.loc[index, column] = (
+                    library_metadata.data.loc[
+                        sheet.loc[index, subcategory_column], column]
+                )
+            # Колонка приоритета сортировки
+            if not isna(sheet.loc[index, priority_column]):
+                library.loc[index, "sort_priority"] = (
+                    sheet.loc[index, priority_column]
+                )
+            else:
+                library.loc[index, "sort_priority"] = default_priority
+            # Колонка возможности наличия множителя
+            if not isna(sheet.loc[index, can_have_multiplier_column]):
+                library.loc[index, "can_have_multiplier"] = True
+            else:
+                library.loc[index, "can_have_multiplier"] = False
+
+            for internal_column, specification_column in {
+                "name": name_column,
+                "description": description_column,
+                "code": code_column,
+                "manufacturer": manufacturer_column,
+                "unit": unit_column,
+                "mass": mass_column,
+                "comment": comment_column
+            }.items():
+                library.loc[index, internal_column] = (
+                    sheet.loc[index, specification_column]
+                )
