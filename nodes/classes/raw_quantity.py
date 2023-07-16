a6539aec7729a5a0c2c14d721e0dc9fa61460d7b
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-07-16 16:32:16 +0300

    get nodes quantity

---
+++ nodes/classes/raw_quantity.py
@@ -0,0 +1,55 @@
+from pandas import isna, DataFrame
+
+
+class RawQuantityOfNodes:
+
+    def __init__(self, dataframe, x, y):
+        table_height = len(dataframe)
+        table_breadth = len(dataframe.columns)
+        self.nodes_quantity = DataFrame()
+
+        # Проверяем что метка не в шапке
+        if y == 0:
+            raise Exception(f"Ключевое слово в первом столбце "
+                            f"на строке {str(x + 2)}")
+
+        if y + 1 >= table_breadth:
+            raise Exception(
+                "Не найдено количество для крайне правого узла"
+            )
+
+        #
+        node_number = 1
+        while (x + node_number < table_height) and (
+                not isna(dataframe.iloc[x + node_number, y])):
+            material = dataframe.iloc[x + node_number, y]
+            number = dataframe.iloc[x + node_number, y + 1]
+
+            # Проверяем корректность данных спецификации
+            if isna(dataframe.iloc[x + node_number, y + 1]):
+                raise Exception(
+                    f"Для {material} отсутствует количество. "
+                    "Метка на строке " +
+                    f"{str(x + 2)} на столбце {str(y + 1)}"
+                )
+            if not isinstance(number, (float, int)):
+                raise Exception(
+                    f"Для {material}"
+                    " количество не число. "
+                    "Метка на строке "
+                    f"{str(x + 2)} на столбце {str(y + 1)}"
+                )
+            # сдвиг координат связан с нумерацией с 0 и
+            # с тем что первая строка уходит под шапку
+
+            # Убираем пустоты с обеих сторон
+            material.strip()
+            self.nodes_quantity.loc[material, "quantity"] = number
+
+            node_number += 1
+
+        if node_number == 1:
+            raise Exception(
+                "Спецификация узла " +
+                f"{dataframe.iloc[x - 1, y - 1]} пуста."
+            )
--- nodes/quantity.py
+++ nodes/quantity.py
@@ -1,4 +1,34 @@
+from common.data_with_keywords import DataWithKeywords
+from .classes.raw_quantity import RawQuantityOfNodes
 
 
 def get_nodes_quantity(path, metadata, quantity_mark):
-    pass
+
+    sheet_list = []
+    for index in metadata.index:
+        if metadata.loc[index, "type"] == "specification":
+            sheet_list.append(index)
+
+    nodes_quantity_list = []
+    nodes_quantity = {}
+
+    for sheet_name in sheet_list:
+        sheet = DataWithKeywords(
+            path,
+            sheet_name,
+            [quantity_mark],
+            tolerate_key_not_found=False
+        )
+
+        for v in sheet.keys[quantity_mark]:
+            nodes_quantity_list.append(
+                RawQuantityOfNodes(sheet.data, v[0], v[1])
+            )
+    for element in nodes_quantity_list:
+        for index in element.nodes_quantity.index:
+            nodes_quantity[index] = (
+                element.nodes_quantity.loc[index, "quantity"] +
+                nodes_quantity.get(index, 0)
+                                    )
+
+    return nodes_quantity
