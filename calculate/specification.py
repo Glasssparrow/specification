82b7e9527003d83b46e47b9ed9dc93b1b032fcbf
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-07-20 23:26:36 +0300

    Update specification.py

--- calculate/specification.py
+++ calculate/specification.py
@@ -1,4 +1,21 @@
+from pandas import DataFrame
 
 
 def get_specification(main, additional, library):
-    pass
+    specification = DataFrame(columns=[
+        "can_have_multiplier",
+        "category_priority",
+        "subcategory_priority",
+        "priority",
+
+        "name",
+        "description",
+        "code",
+        "manufacturer",
+        "unit",
+        "mass",
+        "comment",
+    ]
+    )
+    for k, v in main.items():
+        specification.loc[k, "quality"] = v
