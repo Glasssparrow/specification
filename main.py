a5a8d77657bdd13fbc049c3f813a5f93378b9c27
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-07-23 14:53:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,169 +9,150 @@ from calculate.specification import get_specification
 from print_to_xls.print_to_xls import print_to_xls
 
 
-# Эти данные должны подгружаться из настроек
-main_settings = {
-    "default_priority": 3,
-
-}
-
-library_settings = {
-    # Данные о местонахождении метаданных
-    "library_path": "Library.xlsx",
-    "library_meta_list_name": "Категории",
-
-    # Данные для чтения метаданных
-    "sheet_name_column": "Sheet name",
-    "subcategory_sort_priority_column": "Sort priority",
-    "is_default_subcategory_column": "Is default subcategory",
-    "category_column": "Category",
-    "category_name_column": "Category name",
-    "category_sort_priority_column": "Category priority",
-    "is_default_category_column": "Is default category",
-
-    # Данные для чтения библиотеки
-    "priority_column": "Приоритет",
-    "can_have_multiplier_column": "Есть множитель",
-    "name_column": "Наименование",
-    "description_column": "Обозначение",
-    "code_column": "Код",
-    "manufacturer_column": "Производитель",
-    "unit_column": "Единица измерения",
-    "mass_column": "Масса",
-    "comment_column": "Примечание",
-
-    # Колонка уникальная для локальной библиотеки
-    "subcategory_column": "Категория",
-}
-
-specification_settings = {
-    "specification_meta_list_name": "Метаданные",
-
-    "type_column": "Тип",
-    "specification_keyword": "спецификация",
-    "nodes_keyword": "узлы",
-    "library_keyword": "библиотека",
-
-    "node_mark": "node",
-    "ordered_list_mark": "ol",
-    "unordered_list_mark": "ul",
-    "extra_level_of_numeration_mark": "num",
-    "subnode_mark": "subnode",
-    "quantity_mark": "KEY"
-}
-
-output_settings = {
-    "sheet_name": "test",
-    "file_name": "Test"
-}
-
-# Этот путь должен быть получен от графического интерфейса
-specification_path = "tests/123.xlsx"
-output_path = "tests"
-
-# Получаем данные о данных
-library_meta = get_library_meta(
-    path=library_settings["library_path"],
-    sheet_name=library_settings["library_meta_list_name"],
-    sheet_name_column=library_settings["sheet_name_column"],
-    subcategory_sort_priority_column=(
-        library_settings["subcategory_sort_priority_column"]),
-    is_default_subcategory_column=(
-        library_settings["is_default_subcategory_column"]),
-    category_column=library_settings["category_column"],
-    category_name_column=(
-        library_settings["category_name_column"]),
-    category_sort_priority_column=(
-        library_settings["category_sort_priority_column"]),
-    is_default_category_column=(
-        library_settings["is_default_category_column"]),
-    default_priority=(
-        main_settings["default_priority"]))
-
-nodes_meta = get_nodes_meta(
+def calculate_and_print_specification(
     specification_path,
-    specification_settings["specification_meta_list_name"],
-    specification_settings["type_column"],
-    specification_settings["specification_keyword"],
-    specification_settings["nodes_keyword"],
-    specification_settings["library_keyword"]
-)
 
-# Читаем библиотеку
-library = get_library(
-    metadata=library_meta,
-    default_priority=main_settings["default_priority"],
-    priority_column=library_settings["priority_column"],
-    can_have_multiplier_column=library_settings[
-        "can_have_multiplier_column"],
-    name_column=library_settings["name_column"],
-    description_column=library_settings["description_column"],
-    code_column=library_settings["code_column"],
-    manufacturer_column=library_settings["manufacturer_column"],
-    unit_column=library_settings["unit_column"],
-    mass_column=library_settings["mass_column"],
-    comment_column=library_settings["comment_column"]
-)
-add_local_library(
-    library=library,
-    nodes_metadata=nodes_meta,
-    library_metadata=library_meta,
-    path=specification_path,
-    default_priority=main_settings["default_priority"],
-    subcategory_column=library_settings["subcategory_column"],
-    priority_column=library_settings["priority_column"],
-    can_have_multiplier_column=library_settings[
-        "can_have_multiplier_column"],
-    name_column=library_settings["name_column"],
-    description_column=library_settings["description_column"],
-    code_column=library_settings["code_column"],
-    manufacturer_column=library_settings["manufacturer_column"],
-    unit_column=library_settings["unit_column"],
-    mass_column=library_settings["mass_column"],
-    comment_column=library_settings["comment_column"]
-)
+    library_path,
+    output_path,
+    default_priority,
+    output_setting_file_name,
+    output_setting_sheet_name,
 
-# Читаем узлы
-nodes_list, ol_list, ul_list, num_list, subnodes_list = get_raw_nodes(
-    path=specification_path,
-    metadata=nodes_meta,
-    node_mark=specification_settings["node_mark"],
-    ordered_list_mark=specification_settings["ordered_list_mark"],
-    unordered_list_mark=specification_settings["unordered_list_mark"],
-    extra_level_of_numeration_mark=specification_settings[
-        "extra_level_of_numeration_mark"
-    ],
-    subnode_mark=specification_settings["subnode_mark"]
-)
+    library_setting_meta_list_name,
+    library_setting_sheet_name_column,
+    library_setting_subcategory_sort_priority_column,
+    library_setting_is_default_subcategory_column,
+    library_setting_category_column,
+    library_setting_category_name_column,
+    library_setting_category_sort_priority_column,
+    library_setting_is_default_category_column,
+    library_settings_priority_column,
+    library_setting_can_have_multiplier_column,
+    library_setting_name_column,
+    library_setting_description_column,
+    library_setting_code_column,
+    library_setting_manufacturer_column,
+    library_setting_unit_column,
+    library_setting_mass_column,
+    library_setting_comment_column,
+    library_setting_subcategory_column,
+    library_setting_priority_column,
 
-regular_nodes, special_nodes = get_nodes(
-    nodes_list, ol_list, ul_list, num_list, subnodes_list
-)
+    specification_setting_specification_meta_list_name,
+    specification_setting_type_column,
+    specification_setting_specification_keyword,
+    specification_setting_nodes_keyword,
+    specification_setting_library_keyword,
+    specification_setting_node_mark,
+    specification_setting_ordered_list_mark,
+    specification_setting_unordered_list_mark,
+    specification_setting_extra_level_of_numeration_mark,
+    specification_setting_subnode_mark,
+    specification_setting_quantity_mark,
+):
+    # Получаем данные о данных
+    library_meta = get_library_meta(
+        path=library_path,
+        sheet_name=library_setting_meta_list_name,
+        sheet_name_column=library_setting_sheet_name_column,
+        subcategory_sort_priority_column=(
+            library_setting_subcategory_sort_priority_column),
+        is_default_subcategory_column=(
+            library_setting_is_default_subcategory_column),
+        category_column=library_setting_category_column,
+        category_name_column=(
+            library_setting_category_name_column),
+        category_sort_priority_column=(
+            library_setting_category_sort_priority_column),
+        is_default_category_column=(
+            library_setting_is_default_category_column),
+        default_priority=default_priority
+    )
 
-nodes_quantity = get_nodes_quantity(
-    path=specification_path,
-    metadata=nodes_meta,
-    quantity_mark=specification_settings["quantity_mark"]
-)
+    nodes_meta = get_nodes_meta(
+        path=specification_path,
+        sheet_name=specification_setting_specification_meta_list_name,
+        type_column=specification_setting_type_column,
+        specification_keyword=specification_setting_specification_keyword,
+        nodes_keyword=specification_setting_nodes_keyword,
+        library_keyword=specification_setting_library_keyword,
+    )
 
+    # Читаем библиотеку
+    library = get_library(
+        metadata=library_meta,
+        default_priority=default_priority,
+        priority_column=library_settings_priority_column,
+        can_have_multiplier_column=(
+            library_setting_can_have_multiplier_column),
+        name_column=library_setting_name_column,
+        description_column=library_setting_description_column,
+        code_column=library_setting_code_column,
+        manufacturer_column=library_setting_manufacturer_column,
+        unit_column=library_setting_unit_column,
+        mass_column=library_setting_mass_column,
+        comment_column=library_setting_comment_column,
+    )
+    add_local_library(
+        library=library,
+        nodes_metadata=nodes_meta,
+        library_metadata=library_meta,
+        path=specification_path,
+        default_priority=default_priority,
+        subcategory_column=library_setting_subcategory_column,
+        priority_column=library_setting_priority_column,
+        can_have_multiplier_column=(
+            library_setting_can_have_multiplier_column
+        ),
+        name_column=library_setting_name_column,
+        description_column=library_setting_description_column,
+        code_column=library_setting_code_column,
+        manufacturer_column=library_setting_manufacturer_column,
+        unit_column=library_setting_unit_column,
+        mass_column=library_setting_mass_column,
+        comment_column=library_setting_comment_column
+    )
 
-# Формируем таблицу спецификации
-specification_table = get_materials_dict(
-    nodes_quantity, regular_nodes, special_nodes
-)
-specification_extra = get_dict_of_materials_for_special_nodes(
-    nodes_quantity, special_nodes
-)
-specification = get_specification(
-    main=specification_table,
-    additional=specification_extra,
-    library=library,
-    library_metadata=library_meta,
-)
+    # Читаем узлы
+    nodes_list, ol_list, ul_list, num_list, subnodes_list = get_raw_nodes(
+        path=specification_path,
+        metadata=nodes_meta,
+        node_mark=specification_setting_node_mark,
+        ordered_list_mark=specification_setting_ordered_list_mark,
+        unordered_list_mark=specification_setting_unordered_list_mark,
+        extra_level_of_numeration_mark=(
+            specification_setting_extra_level_of_numeration_mark
+        ),
+        subnode_mark=specification_setting_subnode_mark
+    )
 
-print_to_xls(
-    data=specification,
-    file_name=output_settings["file_name"],
-    path=output_path,
-    sheet_name=output_settings["sheet_name"],
-)
+    regular_nodes, special_nodes = get_nodes(
+        nodes_list, ol_list, ul_list, num_list, subnodes_list
+    )
+
+    nodes_quantity = get_nodes_quantity(
+        path=specification_path,
+        metadata=nodes_meta,
+        quantity_mark=specification_setting_quantity_mark
+    )
+
+    # Формируем таблицу спецификации
+    specification_table = get_materials_dict(
+        nodes_quantity, regular_nodes, special_nodes
+    )
+    specification_extra = get_dict_of_materials_for_special_nodes(
+        nodes_quantity, special_nodes
+    )
+    specification = get_specification(
+        main=specification_table,
+        additional=specification_extra,
+        library=library,
+        library_metadata=library_meta,
+    )
+
+    print_to_xls(
+        data=specification,
+        file_name=output_setting_file_name,
+        path=output_path,
+        sheet_name=output_setting_sheet_name,
+    )
